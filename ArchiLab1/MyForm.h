9df3877260cfa4efcfe230e4ae8e1f24d02476a0
Эмиль Сафин <safinemil32@gmail.com>
2021-12-11 12:27:51 +0200

    j

--- ArchiLab1/MyForm.h
+++ ArchiLab1/MyForm.h
@@ -47,6 +47,13 @@ namespace ArchiLab1 {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::Label^ label9;
+	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::Button^ button1;
 	protected:
 
 	private:
@@ -69,6 +76,13 @@ namespace ArchiLab1 {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label1
@@ -76,25 +90,27 @@ namespace ArchiLab1 {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(12, 75);
+			this->label1->Location = System::Drawing::Point(328, 79);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(51, 20);
+			this->label1->Size = System::Drawing::Size(0, 25);
 			this->label1->TabIndex = 0;
-			this->label1->Text = L"label1";
 			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(71, 12);
+			this->textBox1->Location = System::Drawing::Point(227, 2);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->Size = System::Drawing::Size(132, 22);
 			this->textBox1->TabIndex = 1;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(178, 12);
+			this->textBox2->Location = System::Drawing::Point(227, 36);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->Size = System::Drawing::Size(132, 22);
 			this->textBox2->TabIndex = 2;
 			// 
 			// label2
@@ -102,50 +118,136 @@ namespace ArchiLab1 {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(12, 95);
+			this->label2->Location = System::Drawing::Point(328, 104);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(51, 20);
+			this->label2->Size = System::Drawing::Size(0, 25);
 			this->label2->TabIndex = 7;
-			this->label2->Text = L"label2";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label3->Location = System::Drawing::Point(12, 115);
+			this->label3->Location = System::Drawing::Point(328, 129);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(51, 20);
+			this->label3->Size = System::Drawing::Size(0, 25);
 			this->label3->TabIndex = 8;
-			this->label3->Text = L"label3";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label4->Location = System::Drawing::Point(13, 135);
+			this->label4->Location = System::Drawing::Point(328, 154);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(51, 20);
+			this->label4->Size = System::Drawing::Size(0, 25);
 			this->label4->TabIndex = 9;
-			this->label4->Text = L"label4";
 			// 
 			// listBox1
 			// 
 			this->listBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
 			this->listBox1->FormattingEnabled = true;
-			this->listBox1->ItemHeight = 20;
-			this->listBox1->Location = System::Drawing::Point(17, 158);
+			this->listBox1->ItemHeight = 25;
+			this->listBox1->Location = System::Drawing::Point(25, 194);
+			this->listBox1->Margin = System::Windows::Forms::Padding(4);
 			this->listBox1->Name = L"listBox1";
-			this->listBox1->Size = System::Drawing::Size(388, 404);
+			this->listBox1->Size = System::Drawing::Size(662, 479);
 			this->listBox1->TabIndex = 10;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(12, 2);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(179, 25);
+			this->label5->TabIndex = 11;
+			this->label5->Text = L"Начальный адрес:";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(22, 33);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(169, 25);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"Конечный адрес:";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label7->Location = System::Drawing::Point(17, 79);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(74, 25);
+			this->label7->TabIndex = 13;
+			this->label7->Text = L"Маска:";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label8->Location = System::Drawing::Point(17, 104);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(124, 25);
+			this->label8->TabIndex = 14;
+			this->label8->Text = L"Адрес сети:";
+			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label9->Location = System::Drawing::Point(19, 129);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(276, 25);
+			this->label9->TabIndex = 15;
+			this->label9->Text = L"Широковещательный адрес:";
+			// 
+			// label10
+			// 
+			this->label10->AutoSize = true;
+			this->label10->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label10->Location = System::Drawing::Point(20, 154);
+			this->label10->Name = L"label10";
+			this->label10->Size = System::Drawing::Size(121, 25);
+			this->label10->TabIndex = 16;
+			this->label10->Text = L"МАС-адрес:";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(405, 2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(137, 58);
+			this->button1->TabIndex = 17;
+			this->button1->Text = L"Обработать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(652, 578);
+			this->ClientSize = System::Drawing::Size(701, 711);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label10);
+			this->Controls->Add(this->label9);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
 			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
@@ -153,6 +255,7 @@ namespace ArchiLab1 {
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
@@ -247,13 +350,17 @@ namespace ArchiLab1 {
 				PingReply^ reply = pingSender->Send(msk);
 				if(reply->Status == IPStatus::Success){
 					auto N = System::Net::Dns::GetHostEntry(msk)->HostName;
-					listBox1->Items->Add("Address" + reply->Address->ToString() + "    Ping: " + reply->RoundtripTime + " buf: " + N);
+					listBox1->Items->Add("Address: " + reply->Address->ToString() + "    Ping: " + reply->RoundtripTime + "   Domain: " + N);
 					
 				}
 				begin[begin->Length-1]++;
 			}while(begin[begin->Length - 1] <= end[begin->Length - 1]);
 		}
 		private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e){
+			
+		}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (textBox1->Text != "" && textBox2->Text != "") {
 			System::Net::IPAddress^ a = System::Net::IPAddress::Parse(textBox1->Text);
 			System::Net::IPAddress^ b = System::Net::IPAddress::Parse(textBox2->Text);
 			label1->Text = GetSubnetMask(a, b)->ToString();
@@ -262,5 +369,6 @@ namespace ArchiLab1 {
 			label4->Text = GetMacAddress()->ToString();
 			pingEr();
 		}
-	};
+	}
+};
 }
